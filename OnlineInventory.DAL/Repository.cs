f0a41c4892c0d1f566a536a3fd5bc91963885161
Vignesh L.G <lgvigneshkrishna@outlook.com>
2020-02-13 18:39:17 +0530

    Add project files.

---
+++ OnlineInventory.DAL/Repository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnlineInventory.Entity;
+
+namespace OnlineInventory.DAL
+{
+    public class Repository
+    {
+        public static List<User> list = new List<User>();
+        static Repository()
+        {
+            list.Add(new User { UserName = "vignesh.l.g", password = "Vignesh@1" });
+        }
+        public static IEnumerable<User> GetDetails()
+        {
+            return list;
+        }
+    }
+}
---
+++ OnlineInventory/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineInventory.DAL;
+using OnlineInventory.Entity;
+
+namespace OnlineInventory.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            IEnumerable<User> list = Repository.GetDetails();
+            ViewBag.List = list;
+            TempData["List"] = list;
+            ViewData["List"] = list;
+            return View();
+        }
+    }
+}
